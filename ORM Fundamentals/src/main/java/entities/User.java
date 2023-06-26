f33198a7a104c74e3e9b4a00cbae7d18045ed2e7
Mihail Slavov <mihail_slavov@icloud.com>
2023-06-26 21:41:13 +0300

    ORM

---
+++ ORM Fundamentals/src/main/java/entities/User.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private int age;
+    private LocalDate registrationDate;
+
+
+    public User (String username, String password, int age, LocalDate registrationDate){
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
+
+
